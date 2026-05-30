9699f967bbd7acca90d8317866be334296e9908d
KLewCW <kimberlylew12@gmail.com>
2026-05-30 20:12:29 +0200

    Update f_copulaJAMS_helpers.R

--- R/f_copulaJAMS_helpers.R
+++ R/f_copulaJAMS_helpers.R
@@ -38,8 +38,8 @@ copulaJAMS_pstar <- function(P, cdf){
       u[u == max(u)] <- 1 - 10e-7
       u
     })
-    p.star <- as.matrix(P.star)
-    colnames(P.star) <- colnames(P)
+    P.star <- as.matrix(P.star)
+    colnames(p.star) <- colnames(P)
   }
 
   return(P.star)
@@ -54,4 +54,27 @@ copulaJAMS_correction_cont <- function(P.all, names.endo.regs,cdf){
   #regressors and giving one copula term per endo regressors
 
   #step 1: applying CDF to all regressors (P&W)
+  P.star <- copulaJAMS_pstar(P= P.all, cdf = cdf)
+
+  #step 2: applying qnorm to obtain normal scores C(P) and C(W)
+  C.all <- apply(P.star, 2, qnorm) #using equation C(P_i) = Phi^{-1}(F_hat(P_i)) for each regressor
+
+  #step 3: Estimate variance-covariance matrix \hat{sigma}_{C(P), C(W)}
+  Sigma.hat <- cov(C.all) #equation 21: mentioned that it is the variance-covariance so cov should be used
+  #instead of cor(). i think ?
+
+  #step 4: inverse of the variance-covariance matrix:
+  Sigma.inverse <- solve(Sigma.hat)
+
+  #step 4:finding the d_P copula term to project onto endogenous regressor columns
+  # (I_{dp}, 0_{dw x dp})' selects the d_P endo columns of Sigma^{-1}
+  endo.index <-  which(colnames(C.all) %in% names.endo.regs) # giving the d_P copula terms (one per endo reg)
+
+  #C(P_i, W_i) = (C(P_i)', C(W_i)') \hat(Sigma^{-1}_{C(P), C(W)}) (I_{dp}, 0_{dw x dp})'
+  P.cop <- C.all %*% Sigma.inv[, endo.index, drop = FALSE]
+  P.cop <- as.matrix(P.cop)
+  colnames(P.cop) <- paste0(names.endo.regs, "_cop")
+
+  return(P.cop)
+
 }
