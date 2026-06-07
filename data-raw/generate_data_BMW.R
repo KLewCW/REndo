63ccd9ac3905a6beab819a12d0c5ee4a62566b79
KLewCW <kimberlylew12@gmail.com>
2026-06-07 18:03:08 +0200

    Update generate_data_BMW.R

--- data-raw/generate_data_BMW.R
+++ data-raw/generate_data_BMW.R
@@ -50,3 +50,51 @@ dataCopBMW <- data.frame(y=y, x=x, P=P)
 usethis::use_data(dataCopBMW, overwrite = TRUE)
 
 
+## Dataset for dataCopMultiEndo
+## Each endogenous regressor has its own first-stage regression on X
+## and its own correction term eta_j = Phit^{-1} (F_{e_j} (e_j))
+
+set.seed(123)
+n <- 1000
+
+#True parameters
+#beta0 : 1
+#beta1 : -1
+#gamma1 :  1
+#gamma2: 1
+
+beta0 <- 1
+beta1 <- -1
+gamma1 <- 1
+gamma2 <- 1
+
+
+#From remark 2.1: P_j = delta_j * x + e_j , j in {1,2}
+# e_1, e_2 ~ gamma(1,1). They are independent of each other and of x (assumption A4)
+#u = rho_1 * eta_1 + rho_2 *eta_2 + epsilon
+
+delta1 <- 1
+delta2 <- 1
+rho1 <- 0.5
+rho2 <- 0.5
+
+x <- rgamma(n, shape = 1, rate =1)
+e1 <- rgamma(n, shape =1, rate =1)
+e2 <- rgamma(n, shape = 1, rate = 1)
+
+P1 <- delta1*x + e1
+P2 <- delta2*x + e2
+
+#Equation 2.3 control functions using true gamma(1,1) CDF
+eta1 <- qnorm(pgamma(e1, shape = 1, rate = 1))
+eta2 <- qnorm(pgamma(e2, shape = 1, rate =1))
+
+epsilon <- rnorm(n)
+
+u <- rho1 *eta1 + rho2 *eta2 + epsilon
+
+y <- beta0 + beta1 * x + gamma1 * P1 + gamma2 * P2 + u
+
+dataCopBMWMultiEndo <- data.frame(y =y , x =x, P1 = P1, P2 = P2)
+
+usethis::use_data(dataCopBMWMultiEndo, overwrite = TRUE)
