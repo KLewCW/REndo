98e138c5975f7a5aa35b803fa5b7fb20b2a038f4
Patrik Schilter <patrik.schilter@gmail.com>
2026-06-28 16:53:37 +0200

    Tests: interface param validation (test that uses internal methods)

---
+++ tests/testthat/test-copula2scopenp_interface-validation.R
@@ -0,0 +1,41 @@
+# Method-level parameter verification at the public interface
+# the input checkers are unit-tested separately. Only check that they are being used
+
+skip_on_cran()
+set.seed(42)
+data("dataCopula2sCOPEnpCont")
+
+
+test_that("interface has methods wired in", {
+  df.na <- dataCopula2sCOPEnpCont
+  df.na$P[1] <- NA
+
+  cases <- list(
+    "formula_data" = list(formula = y ~ P + X),
+    "data_basics" = list(data = NULL),
+    "modelframe NA" = list(data = df.na),
+    "npcdistbwargs" = list(npcdistbw.args = list(xdat = 1)),
+    "bws" = list(bws = list(P = 1)),
+    "num.boots" = list(num.boots = -1),
+    "verbose" = list(verbose = NULL)
+  )
+
+  base.args <- list(
+    formula = y ~ P + X | P,
+    data = dataCopula2sCOPEnpCont,
+    npcdistbw.args = list(bwmethod = "normal-reference"),
+    bws = NULL,
+    num.boots = 1000,
+    verbose = FALSE
+  )
+
+  for (nm in names(cases)) {
+    # all from base.args except the one being tested
+    args <- c(cases[[nm]], base.args[!names(base.args) %in% names(cases[[nm]])])
+    expect_error(
+      object = do.call(what = copula2sCOPEnp, args = args),
+      regexp = "The above errors",
+      info = nm
+    )
+  }
+})
