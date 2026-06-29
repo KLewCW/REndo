021f2c5219ec0b36ce80bdec42b00931dde71f61
Patrik Schilter <patrik.schilter@gmail.com>
2026-06-29 10:00:34 +0200

    fit helpers + refactor usage

--- tests/testthat/test-copula2scopenp_interface-runability.R
+++ tests/testthat/test-copula2scopenp_interface-runability.R
@@ -5,28 +5,6 @@ data("dataCopula2sCOPEnpCont")
 data("dataCopula2sCOPEnpBi")
 data("dataCopula2sCOPEnpMulti")
 
-# Fit with standard args while suppressing warning about 1000 boots
-fit_np_fast <- function(
-  formula,
-  data,
-  num.boots = 2,
-  verbose = FALSE,
-  npcdistbw.args = list(bwmethod = "normal-reference"),
-  bws = NULL
-) {
-  return(suppress_lowboots_warning(
-    copula2sCOPEnp(
-      formula = formula,
-      data = data,
-      num.boots = num.boots,
-      verbose = verbose,
-      npcdistbw.args = npcdistbw.args,
-      bws = bws
-    )
-  ))
-}
-
-
 check_clean_fit <- function(res) {
   expect_false(anyNA(fitted(res)))
   expect_false(anyNA(residuals(res)))
@@ -60,7 +38,7 @@ test_that("works for edge-cases", {
   df$P_pos <- abs(df$P) + 1
 
   for (nm in names(cases)) {
-    res <- fit_np_fast(formula = cases[[nm]], data = df)
+    res <- fit_2scopenp_fast(formula = cases[[nm]], data = df)
     check_clean_fit(res)
   }
 })
@@ -68,12 +46,12 @@ test_that("works for edge-cases", {
 test_that("works with backticked variable names", {
   df <- dataCopula2sCOPEnpCont
   df$`P 1` <- df$P
-  res <- fit_np_fast(formula = y ~ `P 1` + X | `P 1`, data = df)
+  res <- fit_2scopenp_fast(formula = y ~ `P 1` + X | `P 1`, data = df)
   check_clean_fit(res)
 })
 
 test_that("works with multiple endo terms", {
-  res <- fit_np_fast(formula = y ~ . | P1 + P2, data = dataCopula2sCOPEnpMulti)
+  res <- fit_2scopenp_fast(formula = y ~ . | P1 + P2, data = dataCopula2sCOPEnpMulti)
   check_clean_fit(res)
   expect_named(res$bws, c("P1", "P2"))
 })
@@ -81,7 +59,7 @@ test_that("works with multiple endo terms", {
 
 # Discrete endo ------------------------------------------------------------
 test_that("works with a discrete endo", {
-  res <- fit_np_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpBi)
+  res <- fit_2scopenp_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpBi)
   check_clean_fit(res)
 })
 
@@ -91,7 +69,7 @@ test_that("works with NA in an irrelevant column", {
   df <- dataCopula2sCOPEnpCont
   df$abc <- rnorm(nrow(df))
   df$abc[1] <- NA
-  res <- fit_np_fast(formula = y ~ P + X | P, data = df)
+  res <- fit_2scopenp_fast(formula = y ~ P + X | P, data = df)
   check_clean_fit(res)
 })
 
@@ -99,7 +77,7 @@ test_that("works with NA in an irrelevant column", {
 # Small sample ----------------------------------------------------------------------
 test_that("works on a small sample", {
   small <- dataCopula2sCOPEnpCont[1:50, ]
-  res <- fit_np_fast(formula = y ~ P + X | P, data = small)
+  res <- fit_2scopenp_fast(formula = y ~ P + X | P, data = small)
   check_clean_fit(res)
 })
 
@@ -147,14 +125,14 @@ test_that("verbose works", {
     )
   )
   expect_no_message(
-    fit_np_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpCont)
+    fit_2scopenp_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpCont)
   )
 })
 
 
 # S3 methods -----------------------------------------------------------
 test_that("S3 methods work", {
-  res <- fit_np_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpCont)
+  res <- fit_2scopenp_fast(formula = y ~ P + X | P, data = dataCopula2sCOPEnpCont)
   expect_no_error(capture_output(print(res)))
   expect_no_error(capture_output(print(summary(res))))
 })
