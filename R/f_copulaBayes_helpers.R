a7a6057cb2493832f9cd3d9428ca9a0cc44cb7f5
KLewCW <kimberlylew12@gmail.com>
2026-07-04 01:33:19 +0200

    Update f_copulaBayes_helpers.R

--- R/f_copulaBayes_helpers.R
+++ R/f_copulaBayes_helpers.R
@@ -1,7 +1,5 @@
-#' @importFrom stats qnorm pnorm qgamma rgamma dnorm
-#' @importFrom mvtnorm rmvnorm
-#' @importFrom LaplacesDemon rdirichlet
-#' @importFrom invgamma dinvgamma
+#' @importFrom stats qnorm pnorm qgamma rgamma dnorm dgamma lgamma
+#' @importFrom MCMCpack rdirichlet
 
 #building margin structure for one variable
 
@@ -65,80 +63,10 @@ copulaBayesDrawLambda <- function(u.channel, mg){
   G.data[is.na(G.data)] <- 0
 
   #Dirichlet(1,...,1) prior: add one Gamma(1,1) pseudo count per cell
+  #posterior : Gamma (1 + n_j, 1) per cell and normalising gives Dir(m; 1 + n_1,..., 1 +n_m)
+  #posterior concentration = Dir(1,...,1) prior + data contribution
+  as.vector(MCMCpack::rdirichlet(1, 1 + G.data))
 
-  G.prior <- rgamma(mg$m, shape = 1, rate = 1)
-
-  G <- G.prior + G.data #posterior : Gamma (1 + n_j, 1) per cell and normalising gives Dir(m; 1 + n_1,..., 1 +n_m)
-
-  G/sum(G)
-}
-
-# Score vectors from Appendix C (W11)
-# This should return N vector of per observation scores for the IWLS proposal
-#eta_i = alpha + x'_i beta + z'_i delta is the linear predictor.
-
-
-#As per W11, score for eta_i [ 0 ... 0 1 /sigma]' (Sigma^{-1} - I ) [ xi_{x,i}, xi_{z,i}, e_i/sigma] + e_i/sigma^2
-#where [0 ... 0 1/sigma]' takes out the last row of (Sigma^{-1} -I) scaled by 1/sigma.
-#this will be represented by A[dim, .]/sigma
-
-#inverse of correlation matrix phi invPhi
-# A = invPhi - I_{dim}
-# xi.z = N x K matrix of normal scores for endogenous regressors
-# xi.x = N x L matrix of normal scores for exogenous regressors
-# e_i = y_i - eta_i, where eta_i = alpha + x'_i beta + z'_i delta (It is a N-vector)
-#sigma^2 is the current error variance
-#dimension(dim) = K+L+1
-
-copulaBayesScoreEta <- function(A, xi.z, xi.x, e, sigma2, dim){
-
-  xi.e <- e/sqrt(sigma2) #standardised error (last element of the full xi vector)
-
-  # let t3_i be the dim-th row of A dotted with the full xi vector
-  # t3_i = A[dim, ]' %*% xi_i
-  #This represents the copula part of the score (the last element from W11)
-  xi.all<- cbind(xi.z, xi.x, xi.e)
-  t3 <- as.vector(xi.all %*% A[dim, ])
-
-  #full score =  1/(sigma) * t3 + e /sigma^2 (element dim from W11)
-
-  (1/sqrt(sigma2)) * t3 + e / sigma2
-
-}
-
-#score and approximate Hessian for log(sigma^2) (Appendix C W12 to W14)
-
-#The function returns score (first derivative) and f2 (second derivative) of
-#log L with respect to log(sigma^2) summed over observations. This is needed to build the Laplace proposal
-#for log(sigma^2)
-
-#From W12, delta (log L_i)/ delta (log sigma^2) has element [dim, dim] = 0.5 * (cross_i * xi.e_i + invPhi33 * xi.e_i ^2) - 0.5
-#where cross_i = sum_{k} A [ dim, k] * xi.z[i,k] + sum_{l} A [dim, K + l] * xi.x[i,l] is the off-diagonal contribution
-
-copulaBayesScorelogsigma2 <- function(A, invPhi33, xi.z, xi.x, e, sigma2){
-  #invPhi33 represents (Xi^{-1})_{33}
-
-  dim <- ncol(xi.z) + ncol(xi.x) + 1
-
-  xi.e <- e/sqrt(sigma2)
-
-  #off diagonal cross terms in row dim of A
-  xi.regs <- cbind(xi.z, xi.x) #works for L=0 because xi.x has 0 columns
-  cross <- as.vector(xi.regs %*% A[dim, seq_len(ncol(xi.regs))])
-
-
-  #score = ( delta (log L ) ) / (delta (log sigma^2)) summed over observations (W12)
-  #score derivative
-  #from eq.7 in Haschka 2025, variance of Structural Error with common hyperparameters a = b = 0.001
-  #where sigma^2~ inverse Gamma prior to the error variance (IG) (a,b)
-  Score <- sum(0.5 * (cross * xi.e + invPhi33 * xi.e^2) - 0.5) - 0.001 + 0.001/sigma2 #sum over obs of W12 + IG prior derivative
-
-
-  #approximate hessian (element [dim, dim] from W14) + IG prior curvature
-
-  f2 <- sum (-0.25 * cross * xi.e - 0.5 * invPhi33 * xi.e^2) - 0.001/sigma2
-
-  list(Score = Score, f2 = f2 )
 }
 
 #Log posterior: equation 4 + priors from equation 5 to 9
@@ -147,36 +75,64 @@ copulaBayesScorelogsigma2 <- function(A, invPhi33, xi.z, xi.x, e, sigma2){
 #numerator/denorminator in the MH step.
 
 
-copulaBayeslogpost <- function( alpha, delta, beta, sigma2, Phi, xi.z, xi.x, sa, sb.delta, sb.beta, y, z, x){
+copulaBayeslogpost <- function(alpha, delta, beta, sigma2, Phi, xi.z, xi.x, sa, sb.delta, sb.beta, y, z, x){
 
-  dim <- ncol(xi.z) + ncol(xi.x) + 1
+  K <- ncol(xi.z)
+  L <- ncol(xi.x)
+  dim <- K + L + 1L
 
   #structural residuals
   e <- y - alpha - z %*% delta - x %*% beta
   xi.e <- pmin(pmax(e/sqrt(sigma2), -8), 8)
 
-  #stacking all normal scores  (Appendix B ordering convention)
+  #stacking all normal scores  (Appendix B ordering convention) (z_1,..,z_K, x_1,.., x_L, e)
   xi.mat <- pmin(pmax(cbind(xi.z, xi.x, xi.e), -8), 8)
 
   # Gaussian copula log density (eq.3)
-  # equation: = -0.5 * log|Phi| - 0.5 * sum_i xi_i' (Phi^{-1} -I) xi_i
+  # equation: log c = -0.5 * log|Phi| - 0.5 * sum_i xi_i' (Phi^{-1} -I) xi_i
   A <- solve(Phi) - diag(dim)
-  log.c <- -0.5 * log(det (Phi)) - 0.5 * sum(apply(xi.mat, 1, function(xi) as.numeric(t(xi) %*% A %*% xi)))
+  log.c <- -0.5 * log(det(Phi)) - 0.5 * sum(apply(xi.mat, 1, function(xi) as.numeric(t(xi) %*% A %*% xi)))
 
 
   #normal structural error log density
   log.e <- sum(dnorm(e, mean =0, sd = sqrt(sigma2), log =TRUE))
 
   #Inverse gamma (0.001, 0.001) prior on sigma2 from eq.7
-  log.s <- invgamma::dinvgamma(sigma2, shape = 0.001, rate = 0.001, log = TRUE)
+  # log f_IG(x; a,b) = log f_G(1/x; a,b) - 2 log(x)
+  log.s <- dgamma(1/sigma2, shape = 0.001, rate = 0.001, log = TRUE) - 2 * log(sigma2)
 
   # 'horseshoe' type shrinkage mentioned in eq. 5 and 6 prior on regression coefficients
   # gamma | phi^2 ~ N(0, phi^2)
   log.a <- dnorm(alpha, mean =0, sd = sqrt(sa), log = TRUE)
   log.d<- sum(dnorm(delta, mean = 0, sd = sqrt(sb.delta), log = TRUE))
-  log.b <-sum(dnorm(beta, mean = 0, sd = sqrt(sb.beta), log = TRUE))
+
+  log.b <-if (length(beta) >0){
+    sum(dnorm(beta, mean = 0, sd = sqrt(sb.beta), log = TRUE))
+  } else{
+    0
+  }
 
   log.c + log.e + log.s + log.a + log.d + log.b
+}
+
+#Reparametrising the logposterior for the random walk MH step
+#theta = c(alpha, delta_1,.., delta_K, beta_1,..., beta_L, log(sigma^2))
+#log jacobian log(sigma^2) = log.sig2 converting from log-scale to natural scale
+copulaBayeslogpostparam <- function(theta, phi, xi.z, xi.x, sa, sb.delta, sb.beta, y,z,x, K,L){
+  alpha <- theta[1L]
+  delta <- theta[seq(2L, 1L + K)]
+  beta < - if (L > 0L){
+    theta[seq(2L + K, 1L + K +L)]
+  } else{
+    numeric(0L)
+  }
+
+  log.s2 <- theta[1L + K + L + 1L]
+  sigma2 <- exp(log.s2)
+
+  #Jacobian = delta sigma^2/ delta log(sigma^2)
+  copulaBayeslogpost(alpha,delta, beta, sigma2, Phi, xi.z, xi.x, sa, sb.delta, sb.beta, y, z, x) + log.s2
+
 }
 
 #Extracting upper triangle of correlation matrix for chain storage
