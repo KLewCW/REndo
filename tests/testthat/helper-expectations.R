9e19fae1c17b999fc154f9e9058156054d10df9c
Patrik Schilter <patrik.schilter@gmail.com>
2026-07-11 13:33:34 +0200

    run_cases(): General purpose case runner

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -1,5 +1,8 @@
-# Methods to apply expect_<X> to multiple test cases
+# check_errmsg() + check_errmsg_empty -------------------------------------------------
+# Mostly for testing the internal methods that return error messages (char vecs) and
+# do not stop()
 
+# Methods to apply expect_<X> to multiple test cases
 # Expect error messages returned
 check_errmsg <- function(fn, base.args = list(), cases, regexp) {
   if (length(regexp) == 1) {
@@ -22,6 +25,7 @@ check_errmsg_empty <- function(fn, base.args = list(), cases) {
 }
 
 # check_clean_fit -------------------------------------------------------------------
+# No NAs anywhere. Basic sanity checks. Mostly used for smoke tests.
 check_clean_fit <- function(res) {
   expect_false(anyNA(fitted(res)))
   expect_false(anyNA(residuals(res)))
@@ -29,7 +33,28 @@ check_clean_fit <- function(res) {
   expect_false(anyNA(vcov(res)))
   expect_false(anyNA(res$boots.params))
 }
-# Suppress boots warning -----------------
+
+# run_cases ------------------------------------------------------------------
+# run `fn` method with `cases` + `base.args` and then apply `check` as a test
+#' @importFrom utils modifyList
+run_cases <- function(fn, base.args = list(), cases, check) {
+  results <- lapply(names(cases), function(nm) {
+    res <- do.call(what = fn, args = modifyList(base.args, cases[[nm]]))
+    withCallingHandlers(
+      check(res),
+      # catch thrown testthat expectation failures
+      expectation_failure = function(e) {
+        e$message <- paste0("[case: ", nm, "] ", e$message)
+        stop(e)
+      }
+    )
+    return(res)
+  })
+  names(results) <- names(cases)
+  return(invisible(results))
+}
+
+# Suppress boots warning -----------------------------------------------------------
 # Suppress 1000 num.boots warning but lets all other warnings propagate
 suppress_lowboots_warning <- function(expr) {
   withCallingHandlers(
@@ -42,7 +67,8 @@ suppress_lowboots_warning <- function(expr) {
   )
 }
 
-# check_param_recovery --------------------------
+# check_param_recovery -------------------------------------------------------------
+# parameter recovery test
 check_param_recovery <- function(res, true_vals) {
   coefs <- coef(res)
   ses <- sqrt(diag(vcov(res)))
@@ -67,11 +93,11 @@ check_struct_residuals <- function(res, aux.names) {
   # Alternative route: Remove cop contribution from augmented fit
   res.lm <- res$res.lm.augmented
 
-  cop.coefs  <- coef(res.lm)[aux.names]
+  cop.coefs <- coef(res.lm)[aux.names]
   cop.matrix <- model.matrix(res.lm)[, aux.names, drop = FALSE]
 
   residuals.alt <- drop(residuals(res.lm) + cop.matrix %*% cop.coefs)
-  fitted.alt    <- drop(fitted(res.lm) - cop.matrix %*% cop.coefs)
+  fitted.alt <- drop(fitted(res.lm) - cop.matrix %*% cop.coefs)
 
   expect_equal(residuals(res), residuals.alt)
   expect_equal(fitted.values(res), fitted.alt)
