a1e9c20e81c353dae5ff589d5efc6126f9b871e0
Patrik Schilter <patrik.schilter@gmail.com>
2026-07-13 21:31:34 +0200

    Use existing methods

--- R/copulajams_helpers.R
+++ R/copulajams_helpers.R
@@ -1,45 +1,3 @@
-#' @importFrom Formula as.Formula
-#' @importFrom stats lm residuals qnorm ecdf cov
-#' @importFrom ks kcde
-#'
-pobs_adj <- function(x) {
-  if (!is.matrix(x)) {
-    x <- matrix(x, ncol = 1)
-  }
-  n <- nrow(x)
-  U <- apply(x, 2, rank, ties.method = "average") * ((n - 1) / n^2) + 1 / (2 * n)
-  return(U)
-}
-
-copulajams_pstar <- function(P, cdf) {
-  if (!is.matrix(P)) {
-    P <- as.matrix(P)
-  }
-
-  if (cdf == "kde") {
-    P.star <- apply(P, 2, function(x) {
-      Fhat <- ks::kcde(x)
-      predict(Fhat, x = x)
-    })
-  } else if (cdf == "resc.ecdf") {
-    P.star <- apply(P, 2, copula::pobs)
-  } else if (cdf == "adj.ecdf") {
-    P.star <- apply(P, 2, pobs_adj)
-  } else {
-    ecdf0 <- apply(P, 2, ecdf)
-    P.star <- sapply(seq_along(ecdf0), function(i) {
-      u <- ecdf0[[i]](P[, i])
-      u[u == min(u)] <- 10e-7
-      u[u == max(u)] <- 1 - 10e-7
-      u
-    })
-    P.star <- as.matrix(P.star)
-    colnames(P.star) <- colnames(P)
-  }
-
-  return(P.star)
-}
-
 copulajams_correction_cont <- function(P.all, names.endo.regs, cdf) {
   #computing the copula correction terms - continuous exogenous case
   # equation 17 : C(P_i, W_i), sigma_(C(P), C(W)} is the variance-covariance matrix
@@ -48,7 +6,7 @@ copulajams_correction_cont <- function(P.all, names.endo.regs, cdf) {
   #regressors and giving one copula term per endo regressors
 
   #step 1: applying CDF to all regressors (P&W)
-  P.star <- copulajams_pstar(P = P.all, cdf = cdf)
+  P.star <- copula_pstar(P = P.all, cdf = cdf)
 
   #step 2: applying qnorm to obtain normal scores C(P) and C(W)
   C.all <- apply(P.star, 2, qnorm) #using equation C(P_i) = Phi^{-1}(F_hat(P_i)) for each regressor
@@ -126,7 +84,7 @@ copulajams_correction_dis <- function(
 
       #Using the steps from equation 21 again:
 
-      P.star <- copulajams_pstar(P = subdat2, cdf = cdf)
+      P.star <- copula_pstar(P = subdat2, cdf = cdf)
 
       C.sub <- apply(P.star, 2, qnorm)
 
