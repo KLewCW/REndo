ddc0f90b37ff0140ce6efd8cc5a3e728f6f018ec
KLewCW <kimberlylew12@gmail.com>
2026-07-25 23:34:48 +0200

    update CDF

--- man-roxygen/template_param_cdf_references.R
+++ man-roxygen/template_param_cdf_references.R
@@ -1,7 +1,7 @@
 #' @references
-#' Becker, JM., Proksch, D., & Ringle, C.M. Revisiting Gaussian
+#' Becker, JM., Proksch, D., & Ringle, C.M. (2022) Revisiting Gaussian
 #' copulas to handle endogenous regressors. \emph{Journal of the Academy of
-#' Marketing Science}, 50, 46--66 (2022).
+#' Marketing Science}, 50, 46--66.
 #' \doi{10.1007/s11747-021-00805-y}
 #'
 #' Liengaard, B. D., Becker, J.-M., Bennedsen, M., Heiler, P., Taylor, L. N.,
@@ -10,6 +10,12 @@
 #' \emph{Journal of the Academy of Marketing Science}, 53(1), 279-299
 #' \doi{10.1007/s11747-024-01055-4}
 #'
-#' Qian, Y., Koschmann, A., and Xie, H. (2024). A practical guide to
-#' endogeneity correction using copulas. NBER Working Paper No. w32231.
-#' \doi{10.2139/ssrn.4754776}
+#' Breitung, J., Mayer, A., & Wied, D. (2024) Asymptotic properties of endogeneity
+#' corrections using nonlinear transformations. \emph{Econometrics Journal}, 27,
+#' 362--383
+#' \doi{10.1093/ectj/utae002}
+#'
+#' Silverman, B.W. (1986). Density Estimation for Statistics and Data Analysis
+#' \doi{10.1201/9781315140919}
+#'
+#'
