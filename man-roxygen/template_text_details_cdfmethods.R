f4495b652ed2b647fc90b2415e2a602e58d515fd
KLewCW <kimberlylew12@gmail.com>
2026-07-25 23:34:48 +0200

    update CDF (picked from integrate BMW branch)

--- man-roxygen/template_text_details_cdfmethods.R
+++ man-roxygen/template_text_details_cdfmethods.R
@@ -22,11 +22,20 @@
 #'    (copulaBMW's Assumption A5).
 #'    }
 #'
-#' \item{cdf="ecdf"}{Standard empirical CDF with \eqn{10^{-7}} boundary replacement,
-#'    giving \eqn{\Phi^{-1}(10^{-7}) = \pm 5.2} for the boundary observations.
+#' \item{cdf="ecdf"}{Standard empirical CDF with an endpoint adjustment. The minimum
+#'      value is replaced by \eqn{10^{-7}} boundary and the maximum by \eqn{1 - 10^{-7}}
+#'      in order to avoid to avoid \eqn{\Phi^{-1}(0) = -\infty} and \eqn{\Phi^{-1}(1) = +\infty}.
+#'      The boundary replacement produces \eqn{\Phi^{-1}(10^{-7}) = \pm 5.2}. Note:
+#'      (Becker et al. 2022) use this adjustment to show that Gaussian copula's performance worsen
+#'      when intercept are included.
 #'    }
 #'
-#' \item{cdf="kde"}{Integral of a density estimator via \code{ks::kcde} used in (Park and Gupta, 2012).}
+#' \item{cdf="kde"}{Integral of a density estimator via \code{ks::kcde} used in (Park and Gupta, 2012).
+#'       \deqn{ \hat{h}(p) = \frac{1}{T \cdot b} \sum_{t=1}^{T} K\!\left(\frac{p - P_t}{b}\right)}
+#'       The CDF is obtained by integrating a Kernel density estimator with and Epanechnikov kernel
+#'       \eqn{K(x) = 0.75 \cdot (1 - x^2) \cdot \mathbb{1} (|x| \leq 1)} and a Silverman (1986) bandwidth
+#'       \eqn{b = 0.9 \cdot T ^{-1/5} \cdot \min(s, \text{IQR}/1.34)} where IQR is the interquantile range,
+#'       \eqn{s} is the data sample standard deviation and \eqn{T} is the number of tme periods observed in the data.}
 #'
 #' }
 #'
