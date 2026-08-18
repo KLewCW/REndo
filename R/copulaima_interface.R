af3fb793013239953f5a565f985b735cc9c3a1d6
KLewCW <kimberlylew12@gmail.com>
2026-08-18 21:59:58 +0200

    Update copulaima_interface.R

--- R/copulaima_interface.R
+++ R/copulaima_interface.R
@@ -119,8 +119,9 @@
 #'
 #' \donttest{
 #' #------------------------------------------------------------------------
-#' # Example 2: Two endogenous regressors with intercept and no exogenous regressor
-#' # True values: mu=10, alpha1 = 1 (P1), alpha2 = 1 (P2)
+#' # Example 2: Two endogenous regressors with intercept and one exogenous regressor
+#' # True values: mu=10, alpha1 = 1 (P1), alpha2 = 1 (P2), ebta = 1 (X)
+#' N = 5000
 #' # Extension of the first example
 #' #------------------------------------------------------------------------
 #' data("dataCopIMAMultiEndo")
