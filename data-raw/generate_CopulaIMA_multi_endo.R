d89d753aaa985321485ac55fd68db9683c7740f4
Patrik Schilter <patrik.schilter@gmail.com>
2026-08-24 00:32:43 +0200

    Revert "Update generate_CopulaIMA_multi_endo.R"
    This reverts commit 4ab6c85e069a86e94e4e7b9187bca9cabc3f1071.

--- data-raw/generate_CopulaIMA_multi_endo.R
+++ data-raw/generate_CopulaIMA_multi_endo.R
@@ -2,44 +2,36 @@
 
 ## Generate dataCopIMAMultiEndo - two continuous endogenous regressors correlated with each other and with one exogenous regressor
 
-set.seed(1234)
+set.seed(123)
 
-n <- 5000
+n <- 1000
 
 # Extension of Haschka (2025) Section 4.1 to the multiple endogenous regressor case
 # This dataset is designed to exercise the multi-endogenous-regressor code path
 # in copulaIMA()
 
 # True parameters
-# 1 exogenous regressor, 1 intercept and 2 endo regressors
-mu <- 10 #intercept
-beta <- 1 #X coefficient
-alpha1 <- 1  #coefficient on endo regressor P1
-alpha2 <- -1   # coefficient on endo regressor P2
-
+# No exogenous regressors but 1 intercept and 2 endogenous regressors
+alpha1 <- 1   # coefficient on endo regressor P1
+alpha2 <- 1   # coefficient on endo regressor P2
+mu <- 10 #true intercept
 
 # Latent Gaussian dependence structure
 # 4 x 4 matrix, extension of eq. 4.2
-# Corr(P1*, P2*) = 0
+# Corr(P1*, P2*) = 0.3 (correlation between the 2 endo regressors)
 # Corr(P1*, eps*) = 0.5  (endogeneity of P1. Same as Haschka's rho = 0.5)
 # Corr(P2*, eps*) = 0.5  (endogeneity of P2)
-# Corr(P1*, X*) = 0.5 P1 correlated with X
-# Corr(P2*, X*) = 0.5 P2 correlated with X
-# Corr (X*, eps*) = 0 X is exogenous (uncorrelated with error)
 
-#(P1*, P2*, X*, eps*)
-Sigma <- matrix(c(1, 0, 0.5,0.5,
-                  0, 1, 0.5, 0.5,
-                  0.5, 0.5, 1, 0,
-                  0.5, 0.5, 0, 1),
-                nrow = 4, ncol = 4, byrow = TRUE)
+Sigma <- matrix(c(1,   0.3, 0.5,
+                  0.3, 1,   0.5,
+                  0.5, 0.5, 1),
+                nrow = 3, ncol = 3)
 
-latent <- MASS::mvrnorm(n = n, mu = rep(0,4), Sigma = Sigma)
+latent <- MASS::mvrnorm(n = n, mu = c(0, 0, 0), Sigma = Sigma)
 
 P1star <- latent[, 1]
 P2star <- latent[, 2]
-Xstar <- latent[, 3]
-epsstar <- latent[, 4]
+epsstar <- latent[, 3]
 
 # Marginal transformations
 # P1 and P2: nonnormal bounded endogenous regressors
@@ -47,14 +39,14 @@ epsstar <- latent[, 4]
 # Values in (0.5, 1.5), ensuring nonnormality for identification
 P1 <- pnorm(P1star) + 0.5
 P2 <- pnorm(P2star) + 0.5
-X <- Xstar + 1 # X is cts
+
 eps <- epsstar #normal
 
-# Outcome equation extension from eq. 4.1 to two endogenous regressors, one exogenous regressor with intercept
-# Y_t = mu + alpha1 * P1_t + alpha2 * P2_t + beta* X_t + eps_t
-y <- mu + beta * X + alpha1 * P1 + alpha2 * P2 + eps
+# Outcome equation extension from eq. 4.1 to two endogenous regressors with intercept
+# Y_t = mu + alpha1 * P1_t + alpha2 * P2_t + eps_t
+y <- mu + alpha1 * P1 + alpha2 * P2 + eps
 
 # Final dataset
-dataCopIMAMultiEndo <- data.frame(y  = y, P1 = P1, P2 = P2, X = X )
+dataCopIMAMultiEndo <- data.frame(y  = y, P1 = P1, P2 = P2)
 
 usethis::use_data(dataCopIMAMultiEndo, overwrite = TRUE)
